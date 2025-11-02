3f522ad82f78d0e39714b74944bf37a122e16df6
dave witten <wittend@wwrinc.com>
2025-11-02 17:10:57 -0600

    Fixing RTD config stuff.

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,11 @@
+# -- Project information -----------------------------------------------------
+project = "mag-usb documentation"
+copyright = "2025, Dave Witten"
+author = "Dave Witten,KD0EAG"
+
 extensions = ["myst_parser"]
 root_doc = "index"
 html_theme = "furo"
+
+# -- Options for EPUB output
+epub_show_urls = "footnote"
